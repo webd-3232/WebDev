3f234072b3871d7d1eb23a15d9bc54aa99346322
webd-3232 <82389595+webd-3232@users.noreply.github.com>
2021-05-24 20:20:21 -0500

    7.0 Samantha
    Website project

--- Version6.0/user18/JS/script.js
+++ Version6.0/user18/JS/script.js
@@ -2,22 +2,10 @@ function changeText() {
     document.getElementById("textChange").innerHTML = "<ul>Thanks for liking my Webpage</u>";
 }
 
-function openWin() {
-    window.open("https://www.w3schools.com","_blank","toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=400, height=400");
-}
-
 function uppercase() {
     var x = document.getElementById("fname");
     x.value = x.value.toUpperCase();
 }
-
-function botton() {
-    document.getElementById("demo").innerHTML = "Hello World";
-  }
-
-  function paragraph() {
-    document.getElementById("demo").innerHTML = "Hello World";
-  }
 
   function displayDate() {
     document.getElementById("demo").innerHTML = Date();
@@ -25,11 +13,6 @@ function botton() {
 
   function highlight(x) {
     x.style.background = "blue";
-  }
-
-  function confirmInput() {
-    fname = document.forms[0].fname.value;
-    alert("Hello " + fname + "! You will now be redirected to www.w3Schools.com");
   }
 
   function page() {
