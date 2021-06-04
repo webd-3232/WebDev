a2eed10f1cd7b3bc306bf7fb83b7ff1b19c7f571
webd-3232 <82389595+webd-3232@users.noreply.github.com>
2021-06-04 08:31:01 -0500

    V6

--- Version6.0/user18/JS/script.js
+++ Version6.0/user18/JS/script.js
@@ -18,3 +18,15 @@ function uppercase() {
   function page() {
     alert("Page is loaded");
   }
+
+  function botton() {
+    document.getElementById("demo").innerHTML = "Hello World";
+  }
+
+  function paragraph() {
+    document.getElementById("dblclick").innerHTML = "You double clicked!";
+  }
+
+  function openWin() {
+    window.open("https://www.w3schools.com","_blank","toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=400, height=400");
+}
